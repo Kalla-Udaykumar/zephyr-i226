8fef4cd6c87e3b603e8f613a68d27be86f956a21
Muhammad Husaini Zulkifli <muhammad.husaini.zulkifli@intel.com>
2023-07-12 11:04:19 -0400

    Update build.sh
    This patch updates the build script based on the latest zephyr i226
    release.
    
    Signed-off-by: Muhammad Husaini Zulkifli <muhammad.husaini.zulkifli@intel.com>

--- setup/idf/build.sh
+++ setup/idf/build.sh
@@ -10,52 +10,37 @@
 # implied warranties, other than those that are expressly stated in the License.
 #-------------------------------------------------------------------------------
 
-# Created by                : aminuddin.jamaluddin@intel.com
-# Description               : This setup script is used to build the sample application
-# Last Modified by          : aminuddin.jamaluddin@intel.com
-# Last Modified Date        : 03/03/2023
-# version                   : 0.0.3
-# Wiki Link                 : https://wiki.ith.intel.com/display/SegEth/Zephyr+Developer+Tests
+# Created by                : muhammad.husaini.zulkifli@intel.com
+# Description               : This setup script is used to clone the IA repo
+# Last Modified by          : muhammad.husaini.zulkifli@intel.com
+# Last Modified Date        : 13/07/2023
+# version                   : 0.0.1
+# Wiki Link                 : https://wiki.ith.intel.com/x/RJvns
 
 platform=$1
-apps="base_eth"
+apps="echo_server"
 
 function header () {
     echo "Initiate Build for "${apps}
 }
 
-function installer() {
-    echo "Adding Dependency:"
-    cd ${WORKSPACE}
-    pip3 install pyelftools
-}
-
-function copy_app () {
-    cd ${WORKSPACE}/ci_repo/sample_app/
-    cp -r ${apps} ${WORKSPACE}/zephyr_ia_build_repo/zephyr/samples/net/
-    cd ${WORKSPACE}/zephyr_ia_build_repo/zephyr/samples/net/${apps}
-    ls -la | grep ${apps}
-    echo "Copying Success"
-}
-
 function build_app () {
     cd ${WORKSPACE}/zephyr_ia_build_repo/zephyr
-    source ./zephyr-env.sh
+    source zephyr-env.sh
     echo "Sample app build in progress"
-    if [ $apps == "base_eth" ]; then
-        west build -d ${WORKSPACE}/zephyr_ia_build_repo/build/${apps}/ -p -b $platform ${WORKSPACE}/zephyr_ia_build_repo/zephyr/samples/net/base_eth
-    #elif [ $apps == "tsn" ]; then
-    #    echo "tsn"
-    #    west build -d ../build/${apps}/ -p -b $platform ${WORKSPACE}/zephyr_ia_build_repo/zephyr/samples/net/tsn
+    if [ $apps == "echo_server" ]; then
+	west build -d ${WORKSPACE}/zephyr_ia_build_repo/build/${apps} -p -b $platform ${WORKSPACE}/zephyr_ia_build_repo/zephyr/samples/net/sockets/${apps}/ -- -DOVERLAY_CONFIG=${WORKSPACE}/zephyr_ia_build_repo/zephyr/overlay-ehl.conf $OPTION
     else
         echo "Sample apps does not exist"
         exit 0
     fi
+    
+    echo "Sample app build in progress final staging"
 
-    # if [ ! -d "${WORKSPACE}/zephyr_ia_build_repo/build/${apps}" ]; then
-    #     echo "Build Failed"
-    #     exit 0
-    # fi
+    if [ ! -d "${WORKSPACE}/zephyr_ia_build_repo/build/${apps}" ]; then
+         echo "Build Failed"
+         exit 0
+    fi
 
     echo "Build Success for: "${apps}
     echo "Copy Binary to upload folder"
@@ -69,13 +54,7 @@ function build_app () {
         echo "Error: zephyr.efi not found. Unable to continue."
         exit 0
     fi
-    cd ${WORKSPACE}
-    [ ! -d "upload" ] && mkdir "upload"
-    cd upload
-    cp -r ${WORKSPACE}/zephyr_ia_build_repo/build/${apps}/zephyr/zephyr.efi .
 }
 
 header
-installer
-copy_app
 build_app
